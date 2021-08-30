5f34632af5498bb080f4ca75e0c1afe7c89e0b8a
Gonzalez, Juan <juan.gonzalez@pnnl.gov>
2021-08-30 16:52:09 -0400

    Added tests to pytest and formatted with black.

--- rct229/ruletest_engine/ruletest_engine.py
+++ rct229/ruletest_engine/ruletest_engine.py
@@ -11,7 +11,6 @@ from rct229.rule_engine.user_baseline_proposed_vals import UserBaselineProposedV
 from rct229.rules.section6 import *
 from rct229.rules.section12 import *
 from rct229.rules.section15 import *
-
 from rct229.ruletest_engine.ruletest_jsons.scripts.json_generation_utilities import (
     merge_nested_dictionary,
 )
--- rct229/ruletest_engine/tests/test_ruletest_engine.py
+++ rct229/ruletest_engine/tests/test_ruletest_engine.py
@@ -1,11 +1,20 @@
 from rct229.ruletest_engine.ruletest_engine import (
     run_lighting_tests,
+    run_receptacle_tests,
     run_transformer_tests,
 )
 
-# def test_run_transformer_tests():
-#
-#     assert run_transformer_tests()
 
-# def test_run_lighting_tests():
-#     assert run_lighting_tests()
+def test_run_transformer_tests():
+
+    assert run_transformer_tests()
+
+
+def test_run_lighting_tests():
+
+    assert run_lighting_tests()
+
+
+def test_run_receptacle_tests():
+
+    assert run_receptacle_tests()
